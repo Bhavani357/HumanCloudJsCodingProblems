527bcd3a4e0a136c2c59cc979cbef03a0e2fa1b3
Durga Bhavani <durgabhavaniyasarla232@gmail.com>
2024-06-02 19:24:19 +0530

    second commit

--- editDistance.js
+++ editDistance.js
@@ -3,8 +3,11 @@ const word2 = 'ros'
 let newWord1 = ''
 let count = 0
 for(let letter of word1){
-    if(word2.includes(letter)){
+    if(!word2.includes(letter)){
+        count += 1
+    }else{
         newWord1 += letter
     }
 }
 console.log(newWord1)
+console.log(count)
