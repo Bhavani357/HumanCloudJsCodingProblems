9a77e8cf3b179aef1da712ecf2d51db85abdc21a
Durga Bhavani <durgabhavaniyasarla232@gmail.com>
2024-06-02 17:50:59 +0530

    first commit

---
+++ longestValidParentheses.js
@@ -0,0 +1,20 @@
+let string = "(()"
+let count = 0
+for(let i=0;i<string.length-1;i++){
+    if(string[i]==="("){
+        if(string[i+1]===")"){
+            count += 2
+        }
+    }
+}
+console.log(count) // 2
+// or 
+let counting = 0
+for(let i=0;i<string.length-1;i++){
+    if(string[i]==="("){
+        if(string.includes(")")){
+            counting += 1
+        }
+    }
+}
+console.log(counting) //2
